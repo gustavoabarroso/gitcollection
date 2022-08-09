f7454a97fd1e95eb301b85447ffd610ee3fbdb7e
gustavoabarroso <g.abreu2504@gmail.com>
2022-08-08 22:38:51 -0300

    feat: started project and added react-router-dom

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Dashboard } from "../pages/Dashboard";
+import { Repo } from "../pages/Repo";
+
+export const Navigation: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/repositories" element={<Repo />} />
+    </Routes>
+  );
+};
