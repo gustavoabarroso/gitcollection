6399273e3b990809f398c4463a1f669eb3123b81
gustavoabarroso <g.abreu2504@gmail.com>
2022-08-09 20:45:54 -0300

    style: dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { Title, Form } from "./styles";
+import logo from "../../assets/logo.svg";
 
 export const Dashboard: React.FC = () => {
-    return <h1>Dashboard</h1>;
-  };
+  return (
+    <>
+      <img src={logo} alt="GitCollection" />
+      <Title>Catálogo de repositórios do Github</Title>
+      <Form>
+        <input placeholder="username/repository_name" />
+        <button type="submit">Buscar</button>
+      </Form>
+    </>
+  );
+};
