448cd3b8d917c7d8121b6a1299eb1f90c55cb3fc
gustavoabarroso <g.abreu2504@gmail.com>
2022-08-10 22:20:10 -0300

    feat: local storage for repositories

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { shade } from "polished";
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -9,7 +13,7 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
   display: flex;
@@ -23,6 +27,12 @@ export const Form = styled.form`
     color: #3a3a3a;
     border-right: 0;
 
+    ${(props) =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+      `}
+      
     &::placeholder {
       color: #a8a8b3;
     }
@@ -91,4 +101,11 @@ export const Repos = styled.div`
       color: #cbcbd6;
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
+  margin-left: 2px;
 `;
